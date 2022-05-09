ca9524e696abe8bf95ce30ba7041d14cf4bf82d2
Isak0v-Vlad <105202703+Isak0v-Vlad@users.noreply.github.com>
2022-05-09 12:19:11 +0300

    Add files via upload

---
+++ main.sh
@@ -0,0 +1,12 @@
+echo "YEAR : 2022.
+
+Hello my name is Vlad. I'm 14 years old.
+I can brag having good experience in coding on Python.
+Now I'm learning Rust.
+
+INSTAGRAM : (isakov08_vlad).
+FACEBOOK : (Vlad Isakov).
+TWITTER : (isakov08_vlad).
+
+PUBLIC DISCORD : (Grey_Admin#6450)
+"

773494a6c46b5dc89adb47b81106beedb9b6e0ac
Isak0v-Vlad <105202703+Isak0v-Vlad@users.noreply.github.com>
2022-05-11 13:55:10 +0300

    Update main.sh

--- main.sh
+++ main.sh
@@ -1,8 +1,9 @@
-echo "YEAR : 2022.
+echo "
+YEAR : 2022.
 
 Hello my name is Vlad. I'm 14 years old.
 I can brag having good experience in coding on Python.
-Now I'm learning Rust.
+Now I'm learning Rust and how to invest into cryptocurrency. 
 
 INSTAGRAM : (isakov08_vlad).
 FACEBOOK : (Vlad Isakov).
